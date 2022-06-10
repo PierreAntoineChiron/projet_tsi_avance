c5c1409fd4db86300f7735199243dac89c556a7d
Pierre-Antoine <pierreantoine.chiron@gmail.com>
2022-06-10 12:29:37 +0200

    carapace

--- main.py
+++ main.py
@@ -39,6 +39,17 @@ def main():
             o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, Transformation3D())
             viewer.add_object(o)
 
+    m =Mesh.load_obj('GreenShell.obj') 
+    m.normalize()
+    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 2, 2, 1]))
+    tr = Transformation3D()
+    tr.translation.y = -np.amin(m.vertices, axis=0)[1]
+    tr.translation.z = -5
+    tr.rotation_center.z = 0.2
+    texture = glutils.load_texture('ItmCommonGShell_auto.png')
+    o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
+    viewer.add_object(o)
+
     viewer.run()
 
 
